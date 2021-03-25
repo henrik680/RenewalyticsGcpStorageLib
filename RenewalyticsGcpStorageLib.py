4b4415a68d8b978d5e8e8e20d1ca96b5e6f1e15c
henrik680 <henrik.martensson@gmail.com>
2021-03-25 22:38:37 +0100

    Updated logg info

--- RenewalyticsGcpStorageLib.py
+++ RenewalyticsGcpStorageLib.py
@@ -4,7 +4,7 @@ import logging
 
 
 def upload_blob_string(bucket_name, data_string, destination_blob_name, metadata, content_type):
-    logging.debug('get_storage_metadata(...) \n\tbucket_name={}\n\tlen data_string={}\n\t'
+    logging.debug('upload_blob_string(...) \n\tbucket_name={}\n\tlen data_string={}\n\t'
                   'destination_blob_name={}\n\tmetadata={}\n\tcontent_type={}'
                   .format(bucket_name, len(data_string), destination_blob_name, metadata, content_type))
     client = storage.Client()
